1d0d3476388e9a8d9fc812085eaeed4c65540282
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-30 11:47:59 +0700

    [FEATURE] Added toggles in Task.vue

--- components/Task.vue
+++ components/Task.vue
@@ -1,9 +1,26 @@
 <template>
     <div :class="`task ${task.done ? 'completed' : ''}`">
-        <div>
-
+        <div class="content" >
+            {{ task.content }}
+        </div>
+        <div class="button">
+            <button @click="toggleDone">{{ task.done ? 'Undo' : 'Done' }}</button>
+            <button @click="toggleDelete" class="delete">Delete</button>
         </div>
     </div>
 </template>
-<script></script>
+
+<script>
+    export default {
+        props : ['task'],
+        methods:{
+            toggleDone(){
+                this.$store.commit('TOGGLE_TASK',this.task)
+            },
+            toggleDelete(){
+                this.$store.commit('DELETE_TASK',this.task)
+            }
+        }
+    }
+</script>
 <style></style>
